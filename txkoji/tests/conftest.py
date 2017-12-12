a4df629aaa59e5df3bd5ff67ea63c1e0dd58f389
Ken Dreyer <kdreyer@redhat.com>
2017-12-12 16:21:47 -0700

    tests: add a test call
    Assert that we can make a call like getUser

--- txkoji/tests/conftest.py
+++ txkoji/tests/conftest.py
@@ -2,6 +2,9 @@ import os
 import pytest
 
 
+pytest_plugins = "pytest_twisted"
+
+
 TESTS_DIR = os.path.dirname(os.path.abspath(__file__))
 FIXTURES_DIR = os.path.join(TESTS_DIR, 'fixtures')
 
---
+++ txkoji/tests/test_call.py
@@ -0,0 +1,34 @@
+from munch import Munch
+import pytest
+from txkoji import Connection
+from twisted.internet import defer
+
+
+class _StubProxy(object):
+    def __init__(self, url):
+        pass
+
+    def callRemote(self, action, payload):
+        """ Return a deferred that always fires successfully """
+        assert action == 'getUser'
+        result = {'id': 2826,
+                  'krb_principal': 'kdreyer@EXAMPLE.COM',
+                  'name': 'kdreyer',
+                  'status': 0,
+                  'usertype': 0}
+        return defer.succeed(result)
+
+
+class TestCall(object):
+
+    @pytest.inlineCallbacks
+    def test_getuser(self, monkeypatch):
+        monkeypatch.setattr('txkoji.Proxy', _StubProxy)
+        koji = Connection('mykoji')
+        user = yield koji.getUser('kdreyer')
+        expected = Munch(id=2826,
+                         krb_principal='kdreyer@EXAMPLE.COM',
+                         name='kdreyer',
+                         status=0,
+                         usertype=0)
+        assert user == expected
