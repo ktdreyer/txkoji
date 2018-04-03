e07683e2ced29c50c1dd13405ff46a9493920afb
Ken Dreyer <kdreyer@redhat.com>
2018-04-03 15:04:26 -0600

    build: add notes about estimate_completion

--- txkoji/build.py
+++ txkoji/build.py
@@ -60,6 +60,23 @@ class Build(Munch):
         """
         Estimate completion time for a build.
 
+        This calls getAverageBuildDuration on the hub for this package. This
+        value is a very rough, an average for all completed builds in the
+        system.
+
+        For now this is better than nothing, but I'm recording a few thoughts
+        here for posterity:
+
+        A more advanced implementation of for estimating build times would
+        track unique averages per build target. Early releases of Ceph "Hammer"
+        versions would build much more quickly than newer Ceph releases like
+        "Mimic", and the current method simply averages all of them.
+
+        Also, different Koji build targets can have different arches. When we
+        build ceph in "newarch" side tags, those build times are wildly beyond
+        the average, but they influence this average too, subtly throwing it
+        off for the x86-only builds that I care about.
+
         :returns: deferred that when fired returns a datetime object for the
                   estimated or actual datetime.
         """
