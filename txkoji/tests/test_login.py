345af4a9bdf3b524849775bca2d39ea87c7e5868
Ken Dreyer <kdreyer@redhat.com>
2018-07-03 14:29:50 -0600

    tests: test an authenticated call
    Prior to this change, we were not testing the code paths that make
    authenticated calls.
    
    Add a test that logs in and calls getLoggedInUser.

--- txkoji/tests/test_login.py
+++ txkoji/tests/test_login.py
@@ -1,8 +1,10 @@
+from munch import Munch
 import pytest
 from twisted.internet import defer
 from txkoji import Connection
 from txkoji import KojiGssapiException
 from txkoji.tests.util import FakeSSLLoginResponse
+from txkoji.tests.util import FakeProxy
 
 
 def fake_post_ok(url, data=None, **kwargs):
@@ -34,3 +36,21 @@ class TestLogin(object):
         koji = Connection('mykoji')
         with pytest.raises(KojiGssapiException):
             yield koji.login()
+
+    @pytest.inlineCallbacks
+    def test_authenticated_call(self, monkeypatch):
+        monkeypatch.setattr('treq_kerberos.post', fake_post_ok)
+        monkeypatch.setattr('txkoji.Proxy', FakeProxy)
+        koji = Connection('mykoji')
+        yield koji.login()
+
+        # cbs call getLoggedInUser \
+        #   --json-output > txkoji/tests/fixtures/calls/getLoggedInUser.json
+        user = yield koji.getLoggedInUser()
+        expected = Munch(status=0,
+                         authtype=2,
+                         name='ktdreyer',
+                         usertype=0,
+                         krb_principal=None,
+                         id=144)
+        assert user == expected
