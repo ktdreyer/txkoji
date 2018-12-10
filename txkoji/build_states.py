1763a21a3a62301e0e973172679e18ccc01ece12
Ken Dreyer <kdreyer@redhat.com>
2018-12-10 15:41:46 -0700

    build_states: add to_str() method

--- txkoji/build_states.py
+++ txkoji/build_states.py
@@ -5,3 +5,18 @@ COMPLETE = 1
 DELETED = 2
 FAILED = 3
 CANCELED = 4
+
+
+def to_str(number):
+    """
+    Convert a build state ID number to a string.
+
+    :param int number: build state ID, eg. 1
+    :returns: state name like eg. "BUILDING", or "(unknown)" if we don't know
+              the name of this build state ID number.
+    """
+    states = globals()
+    for name, value in states.items():
+        if number == value and name.isalpha() and name.isupper():
+            return name
+    return '(unknown state %d)' % number
--- txkoji/tests/test_build_states.py
+++ txkoji/tests/test_build_states.py
@@ -19,3 +19,27 @@ def test_failed():
 
 def test_canceled():
     assert build_states.CANCELED == 4
+
+
+def test_building_to_str():
+    assert build_states.to_str(0) == 'BUILDING'
+
+
+def test_complete_to_str():
+    assert build_states.to_str(1) == 'COMPLETE'
+
+
+def test_deleted_to_str():
+    assert build_states.to_str(2) == 'DELETED'
+
+
+def test_failed_to_str():
+    assert build_states.to_str(3) == 'FAILED'
+
+
+def test_canceled_to_str():
+    assert build_states.to_str(4) == 'CANCELED'
+
+
+def test_unknown_to_str():
+    assert build_states.to_str(999) == '(unknown state 999)'
