7701d0834af5536f2e872bc8182cee4062f9936b
Ken Dreyer <kdreyer@redhat.com>
2019-01-18 09:53:31 -0700

    estimates: multicall containers in average_build_durations()
    With the recent use of multicall in average_build_durations(), we could
    no longer handle container packages. The problem is that we have to
    estimate those with listBuilds, and technically the listBuilds RPC can
    only accept package IDs, not package names:
    https://pagure.io/koji/issue/1209
    
    Update average_build_durations() to check input: all the packages must
    be containers, or none of them must be containers.
    
    If the packages are not containers, then great, we can do a simple
    multicall with getAverageBuildDuration for everything.
    
    If they are containers, then we have to map the packages to package IDs,
    and then listBuilds on every package ID, and then average those build
    times.

--- txkoji/estimates.py
+++ txkoji/estimates.py
@@ -21,19 +21,64 @@ def average_build_duration(connection, package):
     return connection.getAverageBuildDuration(package)
 
 
+@defer.inlineCallbacks
 def average_build_durations(connection, packages):
     """
     Return the average build duration for list of packages (or containers).
 
     :param connection: txkoji.Connection
-    :param list packages: package names
+    :param list packages: package names. These must all be containers, or they
+                          must all be RPMs (do not mix and match.)
     :returns: deferred that when fired returns a KojiMultiCallIterator, which
               has a list of timdelta objects.
     """
+    containers = [name for name in packages if name.endswith('-container')]
+    if len(containers) == len(packages):
+        containers = True
+    elif len(containers) == 0:
+        containers = True
+    else:
+        # This is going to be too complicated to do with multicalls.
+        raise NotImplementedError('cannot mix containers and non-containers')
+
+    if not containers:
+        multicall = connection.MultiCall()
+        for name in packages:
+            multicall.getPackage(name)
+        multicall.getAverageBuildDuration(name)
+        result = yield multicall()
+        defer.returnValue(result)
+
+    # Map all container names to packages (IDs).
     multicall = connection.MultiCall()
-    for pkg in packages:
-        average_build_duration(multicall, pkg)
-    return multicall()
+    names = set(packages)
+    for name in names:
+        multicall.getPackage(name)
+    results = yield multicall()
+    package_map = dict(zip(names, results))
+
+    # List the previous builds for each container.
+    state = build_states.COMPLETE
+    opts = {'limit': 5, 'order': '-completion_time'}
+    multicall = connection.MultiCall()
+    built_packages = []
+    for name in names:
+        package = package_map[name]
+        if package:
+            built_packages.append(name)
+            multicall.listBuilds(package.id, state=state, queryOpts=opts)
+    results = yield multicall()
+    builds_map = dict(zip(built_packages, results))
+
+    package_durations = []
+    for name in packages:
+        builds = builds_map.get(name)
+        average = None
+        if builds:
+            durations = [build.duration for build in builds]
+            average = sum(durations, timedelta()) / len(durations)
+        package_durations.append(average)
+    defer.returnValue(package_durations)
 
 
 @defer.inlineCallbacks
