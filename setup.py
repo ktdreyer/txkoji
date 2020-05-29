314e5bbe6c642fca832e1a9771270881aff05bba
Ken Dreyer <kdreyer@redhat.com>
2020-05-29 11:06:19 -0600

    test py37 and add Python 3.7 to trove classifiers

--- setup.py
+++ setup.py
@@ -152,6 +152,7 @@ setup(
                  'Operating System :: OS Independent',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.6',
+                 'Programming Language :: Python :: 3.7',
     ],
     install_requires=[
         'munch',
